1f41432706f7001ec4fae0386506d5d018633f4e
david <davidofthehelmickclan@yahoo.com>
2018-11-04 14:27:19 -0600

    Added more functionallity to the controller

--- prescription/src/main/java/com/revature/prescription/controller/PrescriptionController.java
+++ prescription/src/main/java/com/revature/prescription/controller/PrescriptionController.java
@@ -2,11 +2,11 @@ package com.revature.prescription.controller;
 
 import java.util.ArrayList;
 
-import org.hibernate.collection.internal.PersistentIdentifierBag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.prescription.entity.Prescription;
@@ -18,13 +18,33 @@ public class PrescriptionController {
 	@Autowired
 	PrescriptionDao dao;
 	
-	@ModelAttribute("prescription/Prescribed")
-	public ArrayList<Prescription> getPrescribed(String status){
-		return dao.findAllByStatus(status);
+	@GetMapping("/prescription/Prescribed")
+	public ArrayList<Prescription> getPrescribed(){
+		return dao.findAllByStatus("Prescribed");
 	}
 	
-	@ModelAttribute("prescription/{id}")
+	@GetMapping("/prescription/{id}")
 	public Prescription getById(@PathVariable("id") long id){
 		return dao.findByPrescriptionID(id);
+	}
+	
+	@ModelAttribute("/prescription/{id}/Approve")
+	public boolean approvePrescription(@PathVariable("id") long id){
+		Prescription found = dao.findByPrescriptionID(id);
+		if (found == null)
+			return false;
+		else if (found.getStatus() != "Prescribed")
+			return false;
+		else{
+			found.setStatus("Distributed");
+			return true;
+		}
+	}
+	
+	@PostMapping("/prescription/{doctor}/{patient}/{drug}/{ammount}")
+	public Prescription newPrescription(@PathVariable("doctor") long doc, @PathVariable("patient") long pat, @PathVariable("drug") long drug, @PathVariable("ammount") long ammount){
+		Prescription prescription = new Prescription(doc, pat, drug, ammount);
+		dao.save(prescription);
+		return prescription;
 	}
 }
