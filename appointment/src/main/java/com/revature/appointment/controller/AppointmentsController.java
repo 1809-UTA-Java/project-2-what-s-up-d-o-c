b09b9c321b32a1b8d77cdb0de1281f83ae878b5d
Daria Davaloo <daria.davaloo@gmail.com>
2018-11-04 23:33:13 -0600

    Added save functionality for html forms

--- appointment/src/main/java/com/revature/appointment/controller/AppointmentsController.java
+++ appointment/src/main/java/com/revature/appointment/controller/AppointmentsController.java
@@ -1,11 +1,15 @@
 package com.revature.appointment.controller;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.appointment.component.AppointmentsComponent;
@@ -16,21 +20,31 @@ import com.revature.appointment.entity.Appointment;
 @RequestMapping("/appointments")
 public class AppointmentsController {
 	AppointmentsComponent appointmentsComponent;
-	
+
 	@Autowired
 	AppointmentsController(AppointmentsComponent appointmentsComponent) {
 		this.appointmentsComponent = appointmentsComponent;
 	}
-	
+
 	@RequestMapping("/getPatientApps/{id}")
-	ArrayList<Appointment> getAppointmentbyPatient(@PathVariable ("id") long patientID) {
+	ArrayList<Appointment> getAppointmentbyPatient(@PathVariable("id") long patientID) {
 		return appointmentsComponent.getAppointmentbyPatient(patientID);
 	}
-	
+
 	@RequestMapping("/getDoctorApps/{id}")
-	ArrayList<Appointment> getAppointmentbyDoctor(@PathVariable ("id") long doctorID) {
+	ArrayList<Appointment> getAppointmentbyDoctor(@PathVariable("id") long doctorID) {
 		return appointmentsComponent.getAppointmentbyDoctor(doctorID);
 	}
-	
+
+	@RequestMapping(value = "/saveAppointment", method = RequestMethod.POST)
+	void saveAppointment(HttpServletRequest req) {
+
+		long patientID = Long.parseLong(req.getParameter("patientID"));
+		long doctorID = Long.parseLong(req.getParameter("doctorID"));
+		Timestamp timeAndDate = Timestamp.valueOf(req.getParameter("timeAndDate"));
+
+		Appointment newApp = new Appointment(doctorID, patientID, timeAndDate);
+		appointmentsComponent.saveAppointment(newApp);
+	}
 
 }
